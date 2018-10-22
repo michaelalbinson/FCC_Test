4d280c9585050e665821dde43c89f118b0a030fd
Michael Albinson <mikealbinson@gmail.com>
2018-10-22 16:12:46 -0400

    Added comments

--- FCC_Test.ino
+++ FCC_Test.ino
@@ -1,20 +1,23 @@
 /*
-Test code for the controller bench test setup
-
-This simple code is only intended to test that the inputs
-and outputs of the controller are connected correctly.
-
-It does not verify the controller process of the controller
+  Test code for the controller bench test setup
+  
+  This simple code is only intended to test that the inputs
+  and outputs of the controller are connected correctly.
+  
+  It does not verify the controller's finite state machine algorithm.
 */
 
+// the digital pin for the system on pin, may be changed as needed.
 #define SYS_ON_PIN 9
+
+// the analog threshold to listen for. May be tuned as needed.
 #define ANALOG_THRESHOLD 500
 
 void setup() {
   // Setup all digital outputs and inputs
   initDPins();
   
-  // turn em all on and off slowly
+  // turn em all on and off quickly
   onOff();
   
   // turn em all on and off slowly
@@ -22,6 +25,7 @@ void setup() {
   delay(1000);
   allOff();
   
+  // do nothing until the system on pin is pressed
   do {
     delay(50);
   while (dread(SYS_ON_PIN) != 1);
@@ -29,8 +33,8 @@ void setup() {
 
 void loop() {
   // check if Ain's read above the constant analog threshold.
-  // turn on a corresponding LED if it is
-  
+  // turn on a corresponding LED if it is 
+  // (A0 -> D8, A1 -> D10, A2 -> D11, A3 -> D12, A4 -> D13
   for (int i = 0; i < 5; i++) {
     if (readA(i) > ANALOG_THRESHOLD) {
       if (i == 0)
@@ -41,10 +45,14 @@ void loop() {
   }
 }
 
+// HELPERS
+
+// Read an analog pin, given by pin
 int readA(int pin) {
   return analogRead(pin);
 }
 
+// Turn a digital pin HIGH (turn it on). Will not work for the system on pin
 void don(int pin) {
   if (pin == SYS_ON_PIN)
     return;
@@ -52,6 +60,7 @@ void don(int pin) {
   digitalWrite(pin, HIGH);
 }
 
+// Turn a digital pin LOW (turn it off). Will not work for the system on pin
 void doff(int pin) {
   if (pin == SYS_ON_PIN)
     return;
@@ -59,6 +68,7 @@ void doff(int pin) {
   digitalWrite(pin, LOW);
 }
 
+// Read from a digital pin. Does not work if <pin> is not the system on pin
 int dread(int pin) {
   if (pin != SYS_ON_PIN)
     return;
@@ -66,6 +76,8 @@ int dread(int pin) {
   return digitalRead(pin);
 }
 
+// initialize all digital pins to be digital outputs, but make the system 
+// on pin a digital input
 void initDPins() {
    for (int i = 2; i < 14; i++) {
       if (i == SYS_ON_PIN)
@@ -77,6 +89,8 @@ void initDPins() {
    pinMode(SYS_ON_PIN, INPUT);
 }
 
+// Turn each digital pins HIGH for half a second, and then turn them back 
+// off one at a time
 void onOff() {
   for (int i = 2; i < 14; i++) {
       if (i == 9)
@@ -88,6 +102,7 @@ void onOff() {
    }
 }
 
+// Turn all of the digital output pins high at the same time
 void allOn() {
   for (int i = 2; i < 14; i++) {
       if (i == SYS_ON_PIN)
@@ -97,6 +112,7 @@ void allOn() {
    }
 }
 
+// Turn all of the digital output pins low at the same time
 void allOff() {
    for (int i = 2; i < 14; i++) {
        if (i == SYS_ON_PIN)
